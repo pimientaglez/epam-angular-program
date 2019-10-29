cf8cb61384b56bb2c4ae231a94fb13af55a8476a
Ricardo Pimienta <Ricardo_Pimienta@epam.com>
2019-10-29 17:59:14 -0600

    Created service to provide mock data

--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CourseServiceService } from '../../services/course-service.service';
 
 @Component({
   selector: 'app-course-list',
@@ -7,40 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-list.component.sass']
 })
 export class CourseListComponent implements OnInit {
-  public courses:Array<Course> = [
-    {
-      id: 1,
-      title:'Video Course 1. Name Tag',
-      creationDate: new Date('11-09-2018'),
-      duration: '1h 28min',
-      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
-    },
-    { 
-      id: 2,
-      title:'Video Course 2. Name Tag',
-      creationDate: new Date('11-09-2018'),
-      duration: '1h 28min',
-      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
-    },
-    { id: 3,
-      title:'Video Course 3. Name Tag',
-      creationDate: new Date('11-09-2018'),
-      duration: '1h 28min',
-      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
-    },
-    { id: 4,
-      title:'Video Course 4. Name Tag',
-      creationDate: new Date('11-09-2018'),
-      duration: '1h 28min',
-      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
-    },
-  ]
-  constructor() { 
+  public courses:Array<Course> 
+
+  constructor(private courseService: CourseServiceService) { 
     console.log('constructor')
   }
 
   ngOnInit() {
     console.log('ngOnInit')
+    this.courses= this.courseService.getCourses();
+    console.log(this.courses);
   }
   
   ngOnChanges()	{
---
+++ src/app/services/course-service.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseServiceService {
+  courses: Array<Course> = [
+    {
+      id: 1,
+      title:'Video Course 1. Name Tag',
+      creationDate: new Date('11-09-2018'),
+      duration: '1h 28min',
+      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
+    },
+    { 
+      id: 2,
+      title:'Video Course 2. Name Tag',
+      creationDate: new Date('11-09-2018'),
+      duration: '1h 28min',
+      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
+    },
+    { id: 3,
+      title:'Video Course 3. Name Tag',
+      creationDate: new Date('11-09-2018'),
+      duration: '1h 28min',
+      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
+    },
+    { id: 4,
+      title:'Video Course 4. Name Tag',
+      creationDate: new Date('11-09-2018'),
+      duration: '1h 28min',
+      description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
+    },
+  ]
+  getCourses():Array<Course>{
+    return this.courses;
+  }
+}
