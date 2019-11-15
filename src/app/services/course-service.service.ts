a4a4ac44ecfe3942addac9fe39178dc81964dabd
Ricardo Pimienta <Ricardo_Pimienta@epam.com>
2019-11-15 00:29:20 -0600

    Adding course-item's topRated behaviour

--- src/app/services/course-service.service.ts
+++ src/app/services/course-service.service.ts
@@ -11,7 +11,7 @@ export class CourseServiceService {
       title:'Video Course 1. Name Tag',
       creationDate: new Date('11-09-2018'),
       duration: '1h 28min',
-      topRated: false,
+      topRated: true,
       description:"Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
     },
     { 
