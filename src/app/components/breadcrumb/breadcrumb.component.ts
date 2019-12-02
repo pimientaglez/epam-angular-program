09025306dba0826bae7f8da9e0803045ff050c1b
Ricardo Pimienta <ricardo_pimienta@epam.com>
2019-12-02 12:02:12 -0600

    Adding navigation logic

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -7,4 +8,11 @@ import { Component, Input } from '@angular/core';
 })
 export class BreadcrumbComponent {
   @Input() section:string;
+  constructor(private router: Router){
+    
+  }
+
+  goBack(){
+    this.router.navigate(['/courses'])
+  }
 }
