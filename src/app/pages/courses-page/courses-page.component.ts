4832f51f1cb9b5d46853a9b1e0aae0887a677f8a
Ricardo Pimienta <ricardo_pimienta@epam.com>
2019-12-02 12:26:04 -0600

    Validating session

--- src/app/pages/courses-page/courses-page.component.ts
+++ src/app/pages/courses-page/courses-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-courses-page',
   templateUrl: './courses-page.component.html',
   styleUrls: ['./courses-page.component.sass']
 })
-export class CoursesPageComponent {
-
+export class CoursesPageComponent implements OnInit{
+  constructor(
+    private auth: AuthService,
+    private router: Router,){
+      
+    }
+    ngOnInit() {
+      if(!this.auth.isAuthenticated()){
+        this.router.navigate(['/login'])
+      }
+    }
 }
--- src/app/pages/new-course/new-course.component.ts
+++ src/app/pages/new-course/new-course.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-course',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCourseComponent implements OnInit {
   section: string = 'New Course';
-  constructor() { }
+  constructor( 
+    private auth: AuthService,
+    private router: Router,) { }
 
   ngOnInit() {
+    if(!this.auth.isAuthenticated()){
+      this.router.navigate(['/login'])
+    }
   }
   createNewCourse(){
     console.log('Create New Course')
