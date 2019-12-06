f3887da615aa7c23fde4966213716e7edf1e0c14
Ricardo Pimienta <Ricardo_Pimienta@epam.com>
2019-12-06 00:54:37 -0600

    Fixing Unit Test

--- src/app/pages/edit/edit.component.spec.ts
+++ src/app/pages/edit/edit.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EditComponent } from './edit.component';
 import { BreadcrumbComponent } from 'src/app/components/breadcrumb/breadcrumb.component';
 import { FormsModule } from '@angular/forms';
 import { DurationPipe } from 'src/app/pipes/duration.pipe';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { CourseServiceService } from 'src/app/services/course-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+class MockActivatedRoute extends ActivatedRoute {
+  constructor() {
+      super();
+      this.params = of({id: 1});
+  }
+}
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
+  let courseService: CourseServiceService;
+  let activatedRoute: ActivatedRoute;
+
   const courseTest = {
     creationDate: new Date('11-09-2018'),
     description: 'Learn about where you can find course descriptions,',
@@ -28,8 +38,12 @@ describe('EditComponent', () => {
       ],
       imports: [
         FormsModule,
-        RouterTestingModule,
-      ]
+        RouterTestingModule
+      ],
+      providers: [{
+        provide: ActivatedRoute,
+        useClass: MockActivatedRoute
+      }]
     })
     .compileComponents();
   }));
@@ -37,11 +51,18 @@ describe('EditComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EditComponent);
     component = fixture.componentInstance;
-    component.course = courseTest;
+    courseService = TestBed.get(CourseServiceService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    const courseSpy = spyOn(courseService, 'getCourseById')
+
+    component.ngOnInit();
+    component.course = courseTest;
+    fixture.detectChanges();
+
+    expect(courseSpy).toHaveBeenCalled();
     expect(component).toBeTruthy();
   });
 });
