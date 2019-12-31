700be5fb4504221f9227426ae92b2d9faafb2aaf
Ricardo Pimienta <ricardo_pimienta@epam.com>
2019-12-31 14:20:00 -0600

    moving currentpath to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import User from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   userInfo: User;
+  path = { current: '' };
+  private subscription: Subscription;
+
   constructor(
     private authService: AuthService,
-    private router: Router, ) {
+    private router: Router,
+    private location: Location,
+    ) {
   }
 
   ngOnInit() {
-    this.router.events.subscribe((val) => {
+    this.subscription = this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
         if (this.authService.isAuthenticated()) {
           this.authService.getUserInfo().subscribe( (res: User) => {
             this.userInfo = res;
           });
         }
-
+        this.path =  { current: this.location.path() };
       }
     });
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/components/user-section/user-section.component.ts
+++ src/app/components/user-section/user-section.component.ts
@@ -1,44 +1,42 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import {Router, NavigationEnd} from '@angular/router';
 import { Location } from '@angular/common';
 import User from 'src/app/models/User';
 import { LoadingService } from 'src/app/services/loading.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
   selector: 'app-user-section',
   templateUrl: './user-section.component.html',
-  styleUrls: ['./user-section.component.sass']
+  styleUrls: ['./user-section.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserSectionComponent implements OnInit, OnChanges {
+export class UserSectionComponent implements OnChanges, OnDestroy  {
   public id: number;
   @Input() userInfo: User;
-  currentPath: string;
+  @Input() path;
   isUserLoggedIn: string;
   firstName: string;
   lastName: string;
+  private subscription: Subscription;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private location: Location,
     private loadingService: LoadingService, ) {
     }
     ngOnChanges(changes: SimpleChanges) {
-      if (changes.userInfo.currentValue) {
-        this.firstName = changes.userInfo.currentValue.name.first;
-        this.lastName = changes.userInfo.currentValue.name.last;
+      console.log(changes);
+      if (changes.userInfo) {
+        if (changes.userInfo.currentValue) {
+          this.firstName = changes.userInfo.currentValue.name.first;
+          this.lastName = changes.userInfo.currentValue.name.last;
+        }
       }
     }
 
-  ngOnInit() {
-    this.router.events.subscribe((val) => {
-      if (val instanceof NavigationEnd) {
-        this.currentPath = this.location.path();
-      }
-    });
-  }
   logOut() {
     this.loadingService.setLoadingStatus(true);
     setTimeout( () => {
@@ -46,5 +44,8 @@ export class UserSectionComponent implements OnInit, OnChanges {
       this.router.navigate(['/login']);
       this.loadingService.setLoadingStatus(false);
     }, 1500 );
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
